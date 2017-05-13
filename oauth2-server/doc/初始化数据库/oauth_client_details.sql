4bee69e29a44148fc4daf63c3cd43f66b71e8be8
zth390872451 <zth390872451@gmail.com>
2017-05-13 12:00:10 +0800

    初始化 数据库 补充表

--- oauth2-server/doc/初始化数据库/oauth_client_details.sql
+++ oauth2-server/doc/初始化数据库/oauth_client_details.sql
@@ -39,3 +39,41 @@ CREATE TABLE `oauth_client_details` (
 -- ----------------------------
 INSERT INTO `oauth_client_details` VALUES ('client_auth_mode', null, '$2a$10$.GCl9SMgNUCavNgdP4hji.bQLl86HCs0gDWSO9BBbXk7Sn9Z0/zLq', 'read,write', 'client_credentials,refresh_token', null, 'USER', '2592000', null, null, null);
 INSERT INTO `oauth_client_details` VALUES ('password_auth_mode', '', '$2a$10$.GCl9SMgNUCavNgdP4hji.bQLl86HCs0gDWSO9BBbXk7Sn9Z0/zLq', 'read,write', 'refresh_token,password', null, 'USER', '2592000', null, null, null);
+
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for ux_member
+-- ----------------------------
+DROP TABLE IF EXISTS `ux_member`;
+CREATE TABLE `ux_member` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_date` datetime NOT NULL,
+  `modify_date` datetime NOT NULL,
+  `client_id` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `app_id` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `birthday` varchar(255) DEFAULT NULL,
+  `cap` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `family_name` varchar(255) DEFAULT NULL,
+  `hobbies` varchar(255) DEFAULT NULL,
+  `location_code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
+  `profession` varchar(255) DEFAULT NULL,
+  `push_type` int(11) DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of ux_member
+-- ----------------------------
+INSERT INTO `ux_member` VALUES ('1', '2017-05-12 17:11:30', '2017-05-12 17:11:33', 'password_mode', '15019271280', 'e10adc3949ba59abbe56e057f20f883e', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
