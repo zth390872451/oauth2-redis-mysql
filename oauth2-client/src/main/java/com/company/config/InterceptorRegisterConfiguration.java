87f19cabf15013b3471dbc01454861f2d2b8adc1
Administrator <zth390872451@gmail.com>
2017-05-14 18:50:21 +0800

    Oauth2认证客户端代码优化

--- oauth2-client/src/main/java/com/company/config/InterceptorRegisterConfiguration.java
+++ oauth2-client/src/main/java/com/company/config/InterceptorRegisterConfiguration.java
@@ -13,12 +13,8 @@ public class InterceptorRegisterConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //match .json api version
 		registry.addInterceptor(new Oauth2Interceptor())
-    	.excludePathPatterns("/api/oauth2/**","/api/member/regist*","/api/member/create","/api/member/upass*","/api/common/msg",
-    			"/api/content/**","/api/common/**","/api/albums/**","/api/common/isExistAccessToken","/api/device/**","/api/video/ackJoinChannel",
-    			"/api/about/gotoPage","/api/charge/**",
-    			"/api/holderStep/step","/api/test/*","/api/sport/info","/monitor/ping","/api/receipt/receiptResult","/api/paipai/**","/api/thirdParty/**","/api/testController/**");
+    	.excludePathPatterns("/api/oauth2/**");
 
     }
 	
